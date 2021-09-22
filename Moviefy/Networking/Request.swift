d87c0a18e1168d734632eca74c580c021a08268d
Alberto <aldo10012@gmail.com>
2021-09-22 13:30:14 -0700

    Began creating networking layer

---
+++ Moviefy/Networking/Request.swift
@@ -0,0 +1,17 @@
+//
+//  Request.swift
+//  Moviefy
+//
+//  Created by Alberto Dominguez on 9/22/21.
+//  Copyright © 2021 Adriana González Martínez. All rights reserved.
+//
+
+import Foundation
+
+struct Request {
+    static let headers = [
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer YOUR_API_KEY"
+    ]
+}
